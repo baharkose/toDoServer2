9558f780628e52d6f20f929a04368ccdbe4135d0
baharkose <baharkse17@gmail.com>
2024-03-25 20:49:55 +0300

    chore: install dependencies

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESSJS - TODO Project with Sequelize
+------------------------------------------------------- */
+
+// dependencies
+// npm i nodemon
+// npm install sequelize sqlite3
