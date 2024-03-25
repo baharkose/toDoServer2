2f9e95a4ac02b5e5f163274dab7bafe040ae9c0c
baharkose <baharkse17@gmail.com>
2024-03-25 20:55:41 +0300

    feat: port events

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESSJS - TODO Project with Sequelize
 ------------------------------------------------------- */
@@ -8,3 +8,16 @@
 // npm i nodemon
 // npm install sequelize sqlite3
 // npm i express dotenv express-async-errors
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000
+
+app.use(express.json());
+
+// catch async-errors
+require("express-async-errors");
+
+app.listen(PORT, () => console.log("Running: http://127.0.0.1" + PORT));
